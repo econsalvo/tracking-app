0c0d10c5209ab3a420324734c9a094a77fdbe947
Elias Consalvo <eliasconsalvo@gmail.com>
2023-10-01 11:31:58 -0400

    Trackig package steps render

--- bringer-test/app/tracking_view/page.jsx
+++ bringer-test/app/tracking_view/page.jsx
@@ -1,28 +1,66 @@
 "use client";
 
 import { Input, Button, Steps } from "antd";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { trackParcel } from "../utils/trackParcel";
 import styles from "./tracking.module.css";
 
 const Tracking = () => {
   const [trackingNumber, setTrackingNumber] = useState("BPS1EP58YI5SKBR");
-  const [stepsData, setStepsData] = useState([
-    "Step 1 Description",
-    "Step 2 Description",
-    "Step 3 Description",
-  ]);
+  const [stepsData, setStepsData] = useState([]);
+
+  const getData = async () => {
+    try {
+      const trackingInfo = await trackParcel(trackingNumber);
+      setStepsData(trackingInfo.parcel_tracking_items);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
       const trackingInfo = await trackParcel(trackingNumber);
-      console.log(trackingInfo);
+      setStepsData(trackingInfo.parcel_tracking_items);
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   };
 
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const formatDateTime = (dateTimeString) => {
+    const options = {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    };
+    return new Date(dateTimeString).toLocaleString(undefined, options);
+  };
+
+  const getTitle = (step) => {
+    const title =
+      step.tracking_code_vendor?.tracking_code_vendor_locales[0]?.description ||
+      step.tracking_code.tracking_code_locales[0].description;
+    return title;
+  };
+
+  const getStatus = (step) => {
+    const status =
+      step.tracking_code_vendor?.tracking_code_vendor_locales[0]?.locale
+        .active || step.tracking_code.tracking_code_locales[0].locale.active;
+    if (status) {
+      return "finish";
+    } else {
+      return "wait";
+    }
+  };
   return (
     <>
       <div className={styles.container}>
@@ -40,12 +78,13 @@ const Tracking = () => {
       </div>
       <div className={styles.stepsContainer}>
         <Steps direction="vertical" current={1} className={styles.centerSteps}>
-          {stepsData.map((stepDescription, index) => (
+          {stepsData.map((step, index) => (
             <Steps.Step
               key={index}
-              title={`Step ${index + 1}`}
-              description={stepDescription}
-              subTitle={"test"}
+              title={getTitle(step)}
+              description={step.location || step.city}
+              subTitle={formatDateTime(step.createdAt)}
+              status={getStatus(step)}
             />
           ))}
         </Steps>
